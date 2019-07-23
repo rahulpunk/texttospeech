cc9899f2093e907ce10886faa9a75bac6fa53b2a
Rahul punk <rahulpunkcute@gmail.com>
2019-07-23 11:08:14 +0530

    Add files via upload

---
+++ texttospeechcode.m
@@ -0,0 +1,14 @@
+% Program to do text to speech.
+% Get user's sentence
+userPrompt = 'What do you want the computer to say?';
+titleBar = 'Text to Speech';
+defaultString = 'Hello World!  MATLAB is an awesome program!';
+caUserInput = inputdlg(userPrompt, titleBar, 10, {defaultString});
+if isempty(caUserInput)
+  return;
+end; % Bail out if they clicked Cancel.
+caUserInput = char(caUserInput); % Convert from cell to string.
+NET.addAssembly('System.Speech');
+obj = System.Speech.Synthesis.SpeechSynthesizer;
+obj.Volume = 100;
+Speak(obj, caUserInput);
